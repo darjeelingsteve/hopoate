972543b46c5a829b56506b4d678c1d606f8cb5cd
Stephen Anthony <steve@darjeelingapps.com>
2020-09-19 16:31:47 +0100

    Add explicit watchOS target

---
+++ Hopoate watchOS/Hopoate.h
@@ -0,0 +1,19 @@
+//
+//  Hopoate.h
+//  Hopoate watchOS
+//
+//  Created by Stephen Anthony on 19/09/2020.
+//  Copyright © 2020 Darjeeling Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Hopoate.
+FOUNDATION_EXPORT double HopoateVersionNumber;
+
+//! Project version string for Hopoate.
+FOUNDATION_EXPORT const unsigned char HopoateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hopoate/PublicHeader.h>
+
+
